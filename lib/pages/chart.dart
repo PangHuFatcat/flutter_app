39d1e4342ddca94bef683e56ca102b271f3d095e
周广俊 <743137213@qq.com>
2021-01-15 15:24:30 +0800

    添加毛玻璃效果

--- lib/pages/chart.dart
+++ lib/pages/chart.dart
@@ -1,9 +1,51 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/rendering.dart';
 
 class Chart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return FrostedDemo();
+  }
+}
+
+class FrostedDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned.fill(
+            child: Container(
+              child: FlutterLogo(),
+            ),
+          ),
+          Positioned(
+            width: 200,
+            height: 200,
+            child: ClipRect(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200.withOpacity(0.5),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Frosted',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
