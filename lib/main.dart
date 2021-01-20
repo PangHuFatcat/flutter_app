f802e7508af2fe6cd92605e43648dd6697843cb1
周广俊 <743137213@qq.com>
2021-01-20 10:59:40 +0800

    隐藏debug标志

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Welcome to Flutter',
       initialRoute: '/',
+      debugShowCheckedModeBanner: false,
       routes: {
         NavigationView.routeName: (context) => NavigationView(),
         Login.routeName: (context) => Login(),
